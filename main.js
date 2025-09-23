fab0f9b4abdbbbb19b7a22be3719d2c007e9f0cc
Josh <joshuapun07@gmail.com>
2025-09-22 20:39:13 -0400

    html (no functionality)

---
+++ main.js
@@ -0,0 +1,10 @@
+function decompose() {
+    const audioFileInput = document.getElementById("audioFileInput");
+    const audioFile = audioFileInput.files;
+    
+    if (audioFile.length > 0) {
+        //if user is uploading their own file
+    } else {
+        //use record feature
+    }
+}

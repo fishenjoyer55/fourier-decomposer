565bda3e3bd3bd2f1b1c04b98a7301174d1af51e
Josh <joshuapun07@gmail.com>
2025-09-23 02:07:49 -0400

    epic audio input

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let hasPermission = false;
+
 function decompose() {
     const audioFileInput = document.getElementById("audioFileInput");
     const audioFile = audioFileInput.files;
@@ -6,5 +8,23 @@ function decompose() {
         //if user is uploading their own file
     } else {
         //use record feature
+    }
+}
+function recordAudio() {
+    window.localAudio = document.getElementById("localAudio");
+    if (hasPermission == false) {
+        navigator.mediaDevices
+        .getUserMedia({ video: false, audio: true })
+        .then((stream) => {
+            window.localStream = stream;
+            window.localAudio.srcObject = stream;
+            window.localAudio.autoplay = true;
+            hasPermission = true;
+        })
+        .catch((err) => {
+            console.error("error getting audio permissions: ${err}");
+        });
+    } else {
+        console.log("yippee");
     }
 }

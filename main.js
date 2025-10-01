5c0e71502d8fca186ddde392fcb2cc09918fcbf5
fishenjoyer55 <gestdec@gmail.com>
2025-10-01 16:16:41 -0400

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ async function decompose() {
 }
 
 function drawWave(canvas, samples) {
-    console.log("drawingwave attempt");
     //lowkey did not know a sample was a single moment in time. i thought a sample was like a 5-second audio
     const ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -22,12 +21,9 @@ function drawWave(canvas, samples) {
     ctx.beginPath();
     const step = Math.ceil(samples.length/canvas.width);
     const axis = canvas.height / 2;
-    console.log("sample length: " + samples.length);
-    console.log("canvas width: " + canvas.width);
     
     //increment p once for every pixel of the canvas. literally pixel by pixel plotting
     for (let p = 0; p < canvas.width; p++) {
-        console.log("pixel here");
         //for each pixel, go to the corresponding section in the sample array and draw a line whose height is the amplitude difference = volume
         ctx.moveTo(p, axis * (1 + Math.min(...samples.slice(p * step, (p + 1) * step))));
         ctx.lineTo(p, axis * (1 + Math.max(...samples.slice(p * step, (p + 1) * step))));

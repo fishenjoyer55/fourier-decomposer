b4dc97b0f6f70c331834a732e38553cb1b5bd22d
fishenjoyer55 <gestdec@gmail.com>
2025-10-08 18:44:35 -0400

    Update main.js
    small tweaks. still no fourier that comes next

--- main.js
+++ main.js
@@ -1,36 +1,10 @@
 let hasPermission = false;
 
+
+const audioFileInput = document.getElementById("audioFileInput");
 const wholeAudioCanvas = document.getElementById("wholeAudioCanvas");
-
-async function decompose() {
-    const audioFileInput = document.getElementById("audioFileInput");
-    const audioFile = audioFileInput.files[0];
-    
-    const audioContext = new AudioContext();
-    const audioBuffer = await audioContext.decodeAudioData(await audioFile.arrayBuffer());
-    const leftSamples = audioBuffer.getChannelData(0);
-
-    drawWave(wholeAudioCanvas, leftSamples);
-}
-
-function drawWave(canvas, samples) {
-    //lowkey did not know a sample was a single moment in time. i thought a sample was like a 5-second audio
-    const ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.strokeStyle = "blue";
-    ctx.beginPath();
-    const step = Math.ceil(samples.length/canvas.width);
-    const axis = canvas.height / 2;
-    
-    //increment p once for every pixel of the canvas. literally pixel by pixel plotting
-    for (let p = 0; p < canvas.width; p++) {
-        //for each pixel, go to the corresponding section in the sample array and draw a line whose height is the amplitude difference = volume
-        ctx.moveTo(p, axis * (1 + Math.min(...samples.slice(p * step, (p + 1) * step))));
-        ctx.lineTo(p, axis * (1 + Math.max(...samples.slice(p * step, (p + 1) * step))));
-    }
-    //who up stroking they 2dcontext
-    ctx.stroke();
-}
+wholeAudioCanvas.addEventListener("click", event => addPlaybar(event.target));
+const fourierTransformCanvas = document.getElementById("fourierTransformCanvas");
 
 function recordAudio() {
     window.localAudio = document.getElementById("localAudio");
@@ -50,3 +24,52 @@ function recordAudio() {
         console.log("yippee");
     }
 }
+
+function makeAudioPlayable(audioFile) {
+    $("#audioSrc").attr("src", URL.createObjectURL(audioFile));
+    document.getElementById("audio").load();
+}
+
+async function graphAudio(audioFile) {
+    const audioBuffer = await new AudioContext().decodeAudioData(await audioFile.arrayBuffer());
+    const samples = audioBuffer.getChannelData(0);
+    drawWave(wholeAudioCanvas, samples);
+}
+
+function drawWave(canvas, samples) {
+    console.log("drawingwave attempt");
+    //lowkey did not know a sample was a single moment in time. i thought a sample was like a 5-second audio
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = "blue";
+    ctx.beginPath();
+    const step = Math.ceil(samples.length/canvas.width);
+    console.log("samples length: " + samples.length);
+    console.log("canvas width: " + canvas.width);
+    
+    //increment p once for every pixel of the canvas. literally pixel by pixel plotting
+    for (let p = 0; p < canvas.width; p++) {
+        //for each pixel, go to the corresponding section in the sample array and draw a line whose height is the amplitude difference (volume). centered and scaled to axis.
+        ctx.moveTo(p, canvas.height / 2 * (1 + Math.min(...samples.slice(p * step, (p + 1) * step))));
+        ctx.lineTo(p, canvas.height / 2 * (1 + Math.max(...samples.slice(p * step, (p + 1) * step))));
+    }
+    //who up stroking they 2dcontext
+    ctx.stroke();
+}
+
+function addPlaybar(canvas) {
+    const newPlaybar = document.createElement("canvas");
+    newPlaybar.classList.add("playbarCanvas");
+    document.getElementById("canvasZone").appendChild(newPlaybar);
+}
+
+$("#audio").on("timeUpdate", () => {
+    console.log("change");
+});
+
+//upload listener
+audioFileInput.addEventListener("input", event => {
+    const audioFile = audioFileInput.files[0];
+    makeAudioPlayable(audioFile);
+    graphAudio(audioFile);
+});
